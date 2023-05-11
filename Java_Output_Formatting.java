8b582f21bda1bb51acde90ffca757a91ca0aaebe
6Gabriel <132931388+6Gabriel@users.noreply.github.com>
2023-05-11 08:24:08 -0400

    Add files via upload

---
+++ Java_Output_Formatting.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.java_output_formatting;
+
+/**
+ *
+ * @author 2022108060039
+ */
+public class Java_Output_Formatting {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
